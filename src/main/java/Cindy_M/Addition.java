eebeb4be9e9fee9b0ad1b44f1763ef96938c57a1
cindymc91 <43747100+cindymc91@users.noreply.github.com>
2018-10-02 13:55:56 +0200

    Add files via upload

---
+++ src/main/java/Cindy_M/Addition.java
@@ -0,0 +1,12 @@
+package Cindy_M;
+
+public class Addition {
+
+    public Long calculer(Long a, Long b) {
+        return a + b;
+    }
+
+    public Character lireSymbole() {
+        return '+';
+    }
+}
